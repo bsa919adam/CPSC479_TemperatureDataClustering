a6ef0faee125acf9ab5cd60bd1795bb32285f4fa
bsa919adam <bsa919adam@alpha.acad.fullerton.edu>
2019-05-07 15:24:08 -0700

    update program

--- main.cu
+++ main.cu
@@ -1,53 +1,100 @@
 
 #include <stdio.h>
 #include <cuda.h>
-#include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define N 32
-__global__ void square(int* a) {
+typedef struct day{
+	int month;
+	int date;
+	int year;
+	double high;
+	double low;
+}day;
+
+
+
+__global__ void setCenters(int* a) {
 	
-	
+	int N=0;
 	for( int i=0; i< N; i++){
 		a[blockIdx.x * N + threadIdx.x] += a[blockIdx.x * N + i] * a[i * N + threadIdx.x];
 	}
 	
 }
 
-void random_ints(int* arr, int row, int colum) {
-    for (int a = 0; a < row; a++) {
-	for( int b=0; b<colum; b++){
-        	arr[a * N + b] = rand() % 100 + 1;
-	}
-    }
+__global__ void kluster(day* data, day* centers, day ** clusters){
+
+
 }
 
-int main(void) {
-    int *a;        // host copy of a
-    int *d_a;    // device copy of a
-    int size = N * N * sizeof(int);
-    srand(time(NULL));
-    printf("line 29 size=%d\n",size);
-    // Alloc space for host copy of a and setup input values
-    a = (int *)malloc(size); 
-    random_ints(a, N, N);
-
-    // Alloc space for device copies of a, b, c
-    cudaMalloc((void **)&d_a, size);
-    // Copy inputs to device
-    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-
-    // Launch add() kernel on GPU with N blocks
-    square<<<N,N>>>(d_a);
-    // Copy result back to host
-    cudaMemcpy(a, d_a, size, cudaMemcpyDeviceToHost);
-    for (int i = 0; i < N; i++) {
-	for(int j = 0; j< N; j++){
-        	printf("a[%d][%d] = %d\n", i,j, a[i*N+j]);
-	}
+int main(int  argc, char *argv[]) {
+    printf("begin checks\n");
+    if(argc < 3){
+	printf("Missing Arguments");
+	return 1;
     }
+    printf("check2\n");
+    int k=atoi(argv[1]);
+    if(k <1){
+	printf("invalid number of Centers");
+	return 1;
+    }
+    printf("check3\n");
+    FILE *fp;
+    fp=fopen(argv[2], "r");
+    if(fp==NULL){
+	perror("Failed to open file:");
+	return 1;
+    }
+    printf("checks done\n");
+    day * data;
+    char c=' ';
+    int n = 0;
+    data=(day*)malloc(sizeof(struct day));
+    printf("starting line skip\n");
+    while((c=fgetc(fp))!='\n'){//getting rid of the title line of the file
+	printf("%c", c);
+        if(c=='\n'){
+		n=1;
+	}
+
+    }
+    printf("line skipped\n");
+    int numDays=0;
+    int high=-1;
+    int low;
+    int date;
+    int month;
+    int year;
+    char  station[15];
+    while(fscanf(fp,"%[,]%*[,]%d%*[/]%d%*[/]%d%*[,]%d%*[,]%d",station,&month, &date, &year, &high, &low)){
+	numDays++;
+	data=(day*)realloc(data, sizeof(struct day) * numDays);
+	data[numDays-1].date=date;
+	data[numDays-1].high=high;
+	data[numDays-1].low=low;
+	data[numDays-1].month=month;
+	data[numDays-1].year=year;
+	printf("%d\n", numDays);
+    }
+    printf("%e\n", data[0].high);
+    printf("%e\n", data[numDays-1].high);
+    
+
+    
+ //   cudaMalloc((void **)&d_a, size);
+    
+   // cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+
+    
+       
+ // cudaMemcpy(a, d_a, size, cudaMemcpyDeviceToHost);
+    
 
     // Cleanup
-    free(a); 
-    cudaFree(d_a); 
+     
+   // cudaFree(d_a);
+    free(data); 
     return 0;
-    }
+}
