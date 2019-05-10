a6470a1760d88c935834f2d8a5f87730ebb8f771
bsa919adam <bsa919adam@alpha.acad.fullerton.edu>
2019-05-09 20:55:18 -0700

    update

--- main.cu
+++ main.cu
@@ -10,8 +10,14 @@ typedef struct day{
 	int year;
 	double high;
 	double low;
+	int cluster=-1;
+	int previous=-1;
 }day;
 
+typedef struct center{
+	int x;
+	int y;
+}center;
 
 
 __global__ void setCenters(int* a) {
@@ -30,33 +36,26 @@ __global__ void kluster(day* data, day* centers, day ** clusters){
 
 int main(int  argc, char *argv[]) {
     printf("begin checks\n");
-    if(argc < 3){
+    if(argc < 3){ //checcks for proper number of Args
 	printf("Missing Arguments");
 	return 1;
     }
-    printf("check2\n");
     int k=atoi(argv[1]);
-    if(k <1){
+    if(k <1){ //checks that k value is greater than 1
 	printf("invalid number of Centers");
 	return 1;
     }
-    printf("check3\n");
     FILE *fp;
     fp=fopen(argv[2], "r");
-    if(fp==NULL){
+    if(fp==NULL){ //chechs that the file opened properly
 	perror("Failed to open file:");
 	return 1;
     }
-    printf("checks done\n");
     day * data;
     char c=' ';
     data=(day*)malloc(sizeof(struct day));
-    printf("starting line skip\n");
-    while((c=fgetc(fp))!='\n'){//getting rid of the title line of the file
-	//printf("%c", c);
-
+    while((fgetc(fp))!='\n'){//getting rid of the title line of the file
     }
-    printf("line skipped\n");
     int numDays=0;
     int high=-1;
     int low;
@@ -64,7 +63,7 @@ int main(int  argc, char *argv[]) {
     int month;
     int year;
     char  station[15];
-    while(fscanf(fp,"%[^,],%d/%d/%d,%d,%d",station,&month, &date, &year, &high, &low)==6){
+    while(fscanf(fp,"%[^,],%d/%d/%d,%d,%d",station,&month, &date, &year, &high, &low)==6){//populates data from file
 	numDays++;
 	data=(day*)realloc(data, sizeof(struct day) * numDays);
 	data[numDays-1].date=date;
@@ -72,16 +71,13 @@ int main(int  argc, char *argv[]) {
 	data[numDays-1].low=low;
 	data[numDays-1].month=month;
 	data[numDays-1].year=year;
-	printf("%d\n", numDays);
     }
-    printf("%e\n", data[0].high);
-    printf("%e\n", data[numDays-1].high);
-    printf("%d\n", numDays);
-
+    day * d_data;
+    //declares data for device
+    cudaMalloc((void **)&d_data, sizeof(struct day)*numDays);
+    cudaMemcpy(d_data, data, sizeof(struct day)*numDays);
+  
     
- //   cudaMalloc((void **)&d_a, size);
-    
-   // cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
 
     
        
@@ -90,7 +86,7 @@ int main(int  argc, char *argv[]) {
 
     // Cleanup
      
-   // cudaFree(d_a);
+    cudaFree(d_data);
     free(data); 
     return 0;
 }
