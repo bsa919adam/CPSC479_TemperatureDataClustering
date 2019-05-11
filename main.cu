82fb9aea8a482bcdf9b7b1bb6214cbb59b1797df
bsa919adam <bsa919adam@gmail.com>
2019-05-11 13:38:36 -0700

    Update main.cu

--- main.cu
+++ main.cu
@@ -203,8 +203,8 @@ int main(int  argc, char *argv[]) {
 		dim3 threads(12, 32);
 		processData<<<k, threads>>>(d_data, d_month_data, k, numDays );
 		cudaMemcpy(month_data, d_month_data, k * 12 *sizeof(int), cudaMemcpyDeviceToHost);
-		fprintf(fp, "Cluster,JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,OCT,NOV,DEC\n");
-		printf("%10s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s\n","Cluster","JAN", "FEB", "MAR","APR","MAY","JUN","JUL","AUG","OCT","NOV","DEC");
+		fprintf(fp, "Cluster,JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC\n");
+		printf("%10s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s%5s\n","Cluster","JAN", "FEB", "MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC");
 		for( int i = 0; i< 12; i++){
 			fprintf(fp, "%d", i+1);
 			printf("10%d", i+1);
